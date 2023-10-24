7ad437ce1809eb5684a50c135f3ce4b385da5797
programer33333 <132077438+programer33333@users.noreply.github.com>
2023-10-24 13:33:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://application--programer33333.repl.co/v1/tasks';
-  const trelloApiRoot = 'http://application--programer33333.repl.co/v1/trello';
+  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
